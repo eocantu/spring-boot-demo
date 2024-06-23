79bdf496b2b29b4cdb8ddbbb411f5901cff5b941
eocantu <eocantu@users.noreply.github.com>
2024-06-23 17:17:15 -0500

    Add string example

---
+++ b-module/src/main/kotlin/com/example/springbootdemo/CommonConstants.kt
@@ -0,0 +1,5 @@
+package com.example.springbootdemo
+
+const val MULTI_LINE_MESSAGE =
+    "This is a multi-line message to show what happens. " +
+            "This is the second line of such message."
